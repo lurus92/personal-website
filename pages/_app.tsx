2587981ead930814afa4e26383556f98627f4cd4
Luigi Russo <lurus92@gmail.com>
2026-02-16 15:18:50 +0100

    Add Google Tag Manager snippet across all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/router';
 import '../styles/globals.css';
 
 const GA_TRACKING_ID = 'G-Q55G0RTMMZ';
+const GTM_CONTAINER_ID = 'GTM-KNSKW8S';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -64,6 +65,23 @@ function MyApp({ Component, pageProps }: AppProps) {
           });
         `}
       </Script>
+      <Script id="gtm-init" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_CONTAINER_ID}');
+        `}
+      </Script>
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${GTM_CONTAINER_ID}`}
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
       <Component {...pageProps} />
     </>
   );
